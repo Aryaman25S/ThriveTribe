aab742253288b941b784bf01d0b3a33deca22520
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2025-02-08 13:22:50 -0500

    start work on firebase auth

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
 from app.routers import user
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ThriveTribe API")
 
 app.include_router(user.router, prefix="/users", tags=["users"])
 
 # (Later, include other routers for groups, tasks, etc.)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost:8081",
+    "http://localhost:19006",
+]
+
+app.add_middleware(
+    CORSMiddleware,  # this middleware will allow requests from the origins
+    allow_origins=origins,  # list of origins that are allowed
+    allow_credentials=True,  # allow credentials
+    allow_methods=["*"],  # allow all methods
+    allow_headers=["*"],  # allow all headers
+)
