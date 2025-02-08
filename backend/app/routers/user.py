9f8c4eda16b1ed7386f370d8181be2f26b2aced8
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2025-02-08 12:46:53 -0500

    add getusers function

--- backend/app/routers/user.py
+++ backend/app/routers/user.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 router = APIRouter()
+
 
 @router.get("/users")
 async def read_users():
     return {"message": "Read users"}
+
+
+@router.get("/signup")
+async def signup(usrname: str, email: str, password: str):
+    return {"message": "Signed up successfully"}
