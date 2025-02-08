48e52fcee57e6e20de84a1f101c2f490ec0c68f7
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2025-02-08 12:46:53 -0500

    add getusers function

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.routers import user
 
-app = FastAPI()
+app = FastAPI(title="ThriveTribe API")
 
-app.include_router(user.router)
+app.include_router(user.router, prefix="/users", tags=["users"])
+
+# (Later, include other routers for groups, tasks, etc.)
