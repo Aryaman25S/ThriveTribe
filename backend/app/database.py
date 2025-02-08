12e219d00b703dbd24b77f491897d4e13a910088
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2025-02-08 16:35:01 -0500

    create db

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,6 +3,9 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL", "postgresql+asyncpg://myuser:password@localhost/postgres"
