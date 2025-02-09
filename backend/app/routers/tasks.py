98fe1bccfeb405b01253151e32ea604fda312d56
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2025-02-09 11:06:56 -0500

    show prev tasks

--- backend/app/routers/tasks.py
+++ backend/app/routers/tasks.py
@@ -89,6 +89,33 @@ async def get_task(task_id: int, db: AsyncSession = Depends(get_db)):
     return task
 
 
+@router.get("/getprevious/{user_name}", response_model=TaskResponse)
+async def get_previous_tasks(user_name: str, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(User).filter(User.user_name == user_name))
+    user = result.scalar_one_or_none()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    now = datetime.now()
+    start_of_day = datetime(now.year, now.month, now.day, 0, 0, 0)
+    end_of_day = start_of_day + timedelta(days=1)
+
+    result = await db.execute(
+        select(Task)
+        .filter(Task.assigned_to == user.id)
+        .filter(Task.created_at < start_of_day)
+    )
+    # Get the first task
+    task = result.scalar_one_or_none()
+    if not task:
+        # Select a random task from the static_task table
+        result = await db.execute(select(Task).filter(Task.assigned_to == 0))
+        task = result.scalar_one_or_none()
+        if not task:
+            raise HTTPException(status_code=404, detail="No tasks found")
+    return task
+
+
 @router.patch("/update/{task_id}", response_model=TaskResponse)
 async def update_task(
     task_id: int, task_update: TaskUpdate, db: AsyncSession = Depends(get_db)
